4926cd416132c8bc2bebd8e0bc1714a0b9dc4fec
Israel <3ti2025@gmail.com>
2025-05-19 09:43:08 -0300

    07- adicionadas imagens na pasta images - criação do objeto shows - criação do arquivo montarFront.js - importação do objeto show

---
+++ js/objetos.js
@@ -0,0 +1,241 @@
+export const shows = [{
+        artista: "Foo Fighters",
+        local: "São Paulo",
+        data: "2025-06-05",
+        horario: "21:00",
+        tipo_ingresso: "vip",
+        banner: "../images/image0.jpg"
+    },
+    {
+        artista: "Red Hot Chili Peppers",
+        local: "Rio de Janeiro",
+        data: "2025-06-10",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Linkin Park",
+        local: "Brasília",
+        data: "2025-06-15",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "Green Day",
+        local: "Belo Horizonte",
+        data: "2025-06-20",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "Metallica",
+        local: "Curitiba",
+        data: "2025-06-25",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "Pearl Jam",
+        local: "Fortaleza",
+        data: "2025-07-01",
+        horario: "21:00",
+        tipo_ingresso: "vip",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "The Strokes",
+        local: "Recife",
+        data: "2025-07-06",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "Arctic Monkeys",
+        local: "Salvador",
+        data: "2025-07-11",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "Nirvana",
+        local: "Porto Alegre",
+        data: "2025-07-16",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "Oasis",
+        local: "Manaus",
+        data: "2025-07-21",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image9.jpg"
+    },
+    {
+        artista: "Muse",
+        local: "João Pessoa",
+        data: "2025-07-26",
+        horario: "21:00",
+        tipo_ingresso: "vip",
+        banner: "../images/image0.jpg"
+    },
+    {
+        artista: "Paramore",
+        local: "Natal",
+        data: "2025-08-01",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Kings of Leon",
+        local: "Belém",
+        data: "2025-08-06",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "System of a Down",
+        local: "Teresina",
+        data: "2025-08-11",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "The Killers",
+        local: "Palmas",
+        data: "2025-08-16",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "R.E.M.",
+        local: "Maceió",
+        data: "2025-08-21",
+        horario: "21:00",
+        tipo_ingresso: "vip",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "Coldplay",
+        local: "Aracaju",
+        data: "2025-08-26",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "The Smashing Pumpkins",
+        local: "São Luís",
+        data: "2025-09-01",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "Imagine Dragons",
+        local: "Cuiabá",
+        data: "2025-09-06",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "The Offspring",
+        local: "Campo Grande",
+        data: "2025-09-11",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image9.jpg"
+    },
+    {
+        artista: "Scorpions",
+        local: "Porto Velho",
+        data: "2025-09-16",
+        horario: "21:00",
+        tipo_ingresso: "vip",
+        banner: "../images/image0.jpg"
+    },
+    {
+        artista: "Blink-182",
+        local: "Boa Vista",
+        data: "2025-09-21",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Queens of the Stone Age",
+        local: "Macapá",
+        data: "2025-09-26",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "Evanescence",
+        local: "Rio Branco",
+        data: "2025-10-01",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "U2",
+        local: "Florianópolis",
+        data: "2025-10-06",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "Guns N' Roses",
+        local: "São Paulo",
+        data: "2025-10-11",
+        horario: "21:00",
+        tipo_ingresso: "vip",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "The Rolling Stones",
+        local: "Rio de Janeiro",
+        data: "2025-10-16",
+        horario: "21:00",
+        tipo_ingresso: "pista",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "The Who",
+        local: "Brasília",
+        data: "2025-10-21",
+        horario: "21:00",
+        tipo_ingresso: "meia entrada",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "AC/DC",
+        local: "Belo Horizonte",
+        data: "2025-10-26",
+        horario: "21:00",
+        tipo_ingresso: "premium",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "Led Zeppelin",
+        local: "Curitiba",
+        data: "2025-11-01",
+        horario: "21:00",
+        tipo_ingresso: "inteira",
+        banner: "../images/image9.jpg"
+    }
+];
